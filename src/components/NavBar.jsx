ec4666b5d97a41c0c51ed122667fcf3f98fe41b4
Emmylums <emmanuellumati22@gmail.com>
2026-01-15 16:06:31 +0100

    Logo setting

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import logo from '/logo.png';
+import logo from '../assets/logo.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 import {Element} from 'react-scroll';
